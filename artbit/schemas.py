a5df4c7eb166b44e3537aa24d55d783ae126c0c4
Eelis Kostiainen <eelis@huuva.io>
2024-11-06 00:18:08 +0200

    Remove redundant tests

--- artbit/schemas.py
+++ artbit/schemas.py
@@ -3,6 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Heartbeat:
-    # TODO: add sender
     timestamp: str
     bpm: int
--- test/conftest.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import pytest
-
-
-@pytest.fixture(scope="module", autouse=True)
-def setup_pygame():
-    pygame.mixer.init(frequency=44100, size=-16, channels=2)
-    yield
-    pygame.mixer.quit()
